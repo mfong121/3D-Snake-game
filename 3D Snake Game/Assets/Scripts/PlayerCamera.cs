671cac9029779e4c2578afdae959d57740e0e5f6
mfong121 <fong.121@osu.edu>
2021-12-07 17:56:18 -0500

    Moved camera controls into playerController

--- 3D Snake Game/Assets/Scripts/PlayerCamera.cs
+++ 3D Snake Game/Assets/Scripts/PlayerCamera.cs
@@ -1,121 +1,15 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(PlayerController))]
 public class PlayerCamera : MonoBehaviour
 {
 
     [SerializeField]
-    public Camera camera;
-    PlayerController playerController;
-    private PlayerControls playerControls;
-
-
-    //Needs to be lower for larger objects, smaller for small objects
-    private float keyboardRotationSpeedInDegrees = 1f;
-    private float mouseRotationSpeedInDegrees = 2f;
-    private float boostRotationModifier = 4;
-    private float rollAngle = 0;
-    private float rollDirection = 0;
+    public Transform target;
     
-    //Desired rotation
-    private Vector3 rotationDirection;
-    //private Quaternion rotationDirection;
-
-    private void Awake()
-    {
-        playerControls = new PlayerControls();
-        playerController = transform.GetComponent<PlayerController>();
-        boostRotationModifier = 2*playerController.boostMultiplier;
-    }
-    private void OnEnable()
-    {
-        
-        playerControls.Player.MouseCameraRotation.performed += mouseRotateCameraActionPerformed;
-        playerControls.Player.KeyboardCameraRotation.performed += keyboardRotateCameraActionPerformed;
-
-        playerControls.Player.MouseCameraRotation.canceled += RotateCameraActionCancelled;
-        playerControls.Player.KeyboardCameraRotation.canceled += RotateCameraActionCancelled;
-
-        if (playerController.mouseControlsActive)
-        {
-            playerControls.Player.MouseCameraRotation.Enable();
-        }
-        else
-        {
-            playerControls.Player.KeyboardCameraRotation.Enable();
-        }
-        playerControls.Player.Roll.performed += rollActionPerformed;
-        playerControls.Player.Roll.canceled += rollActionCancelled;
-        playerControls.Player.Roll.Enable();
-
-    }
-
     // LateUpdate is called once per frame after normal Update calls
     void LateUpdate()
     {
         //move camera to follow player
-        camera.transform.position = transform.position - camera.transform.forward * 4 + camera.transform.up * 1;
-
-        //sets rotation speed to mouseRotationSpeed if mouseControlsActive, and keyboardRotationSpeed * boostModifier if boosting, or *1 if not.
-        var cameraRotationSpeed = playerController.mouseControlsActive ? mouseRotationSpeedInDegrees : keyboardRotationSpeedInDegrees * (playerController.speedBoostActive ? boostRotationModifier : 1);
-        //player head should follow camera slowly instead
-        var newDirection = Vector3.RotateTowards(transform.position, transform.position + rotationDirection, cameraRotationSpeed, 0);
-
-        camera.transform.rotation = Quaternion.LookRotation(newDirection);
-        /*camera.transform.rotation = Quaternion.RotateTowards(transform.rotation,transform.rotation * rotationDirection, cameraRotationSpeed);*/
-        
-        rollAngle = rollDirection * keyboardRotationSpeedInDegrees;
-        camera.transform.Rotate(camera.transform.forward, rollAngle);
-        
-    }
-
-    private void keyboardRotateCameraActionPerformed(InputAction.CallbackContext obj)
-    {
-        //rotate camera according to player turning speed
-        //SHOULD ONLY BE ACTIVE WHEN MOUSECONTROLS DISABLED
-        Vector2 rotationInput = playerControls.Player.KeyboardCameraRotation.ReadValue<Vector2>();
-        float currentRotationSpeed = keyboardRotationSpeedInDegrees;
-        if (playerController.speedBoostActive)
-        {
-            currentRotationSpeed *= boostRotationModifier;
-        }
-        /*rotationDirection = Quaternion.Euler(-rotationInput.y, rotationInput.x,0);*/
-        rotationDirection = new Vector3(rotationInput.x, rotationInput.y, 0);
-        /*transform.rotation = Quaternion.RotateTowards(transform.rotation, transform.rotation * Quaternion.Euler(rotationInput.y, rotationInput.x, 0), currentRotationSpeed);*/
-
-    }
-    private void mouseRotateCameraActionPerformed(InputAction.CallbackContext obj)
-    {
-        //boost should not affect camera rotation speed, only player turning speed
-        Vector2 deltaMousePos = playerControls.Player.MouseCameraRotation.ReadValue<Vector2>();
-
-        //Rotates the camera towards the input
-        /*rotationDirection = Quaternion.Euler(-deltaMousePos.y, deltaMousePos.x, 0);*/
-        rotationDirection = new Vector3(deltaMousePos.x, deltaMousePos.y, 0);
-
-
-/*        transform.rotation = Quaternion.RotateTowards(transform.rotation, transform.rotation * Quaternion.Euler(-deltaMousePos.y, deltaMousePos.x, 0), rotationSpeedInDegrees);*/
-
-        /*var rotationDirection = Vector3.RotateTowards(transform.forward, new Vector3(transform.position.x + movementVector.x, transform.position.y + movementVector.y, transform.position.z), rotationSpeedInDegrees, 0);*/
-        /*transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);*/
-
-        /*Debug.DrawRay(transform.position, rotationDirection, Color.red);
-        transform.rotation = Quaternion.LookRotation(rotationDirection);*/
-    }
-
-    private void RotateCameraActionCancelled(InputAction.CallbackContext obj)
-    {
-        /*        rotationDirection = Quaternion.Euler(0, 0, 0);*/
-        rotationDirection = new Vector3();
-    }
-    private void rollActionPerformed(InputAction.CallbackContext obj)
-    {
-        rollDirection = playerControls.Player.Roll.ReadValue<float>();
-        /*transform.Rotate(transform.forward, rollDirection * keyboardRotationSpeedInDegrees, Space.World);*/
-    }
-    private void rollActionCancelled(InputAction.CallbackContext obj)
-    {
-        rollDirection = 0;
-    }
+        transform.position = target.transform.position - transform.forward * 4 + transform.up * 1;       
+    }    
 }
--- 3D Snake Game/Assets/Scripts/PlayerController.cs
+++ 3D Snake Game/Assets/Scripts/PlayerController.cs
@@ -13,31 +13,83 @@ public class PlayerController: MonoBehaviour
     public bool mouseControlsActive = true;
     [SerializeField]
     private Camera camera; //This camera determines the player's rotation and direction
+    [SerializeField]
+    private Transform head;
 
+
+    //Desired rotation
+    /*private Vector3 rotationDirection;*/
+    private Quaternion rotationDirection;
 
     internal float boostMultiplier = 2f;
     internal bool speedBoostActive = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, transform.position + transform.forward, moveSpeed * Time.deltaTime);
-        //player should turn towards camera rotation & be affected by size, boost
+    private float keyboardRotationSpeedInDegrees = .5f;
+    private float mouseRotationSpeedInDegrees = .01f;
 
-        //TODO: need to turn player transform rotation to match camera transform rotation
-    }
+    private float rollAngle = 0;
+    private float rollDirection = 0;
+
     void Awake()
     {
         playerControls = new PlayerControls();
     }
 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+/*        transform.position = Vector3.MoveTowards(transform.position, transform.position + transform.forward, moveSpeed * Time.deltaTime);*/
+        //player should turn towards camera rotation & be affected by size, boost
+
+
+        //sets rotation speed to mouseRotationSpeed if mouseControlsActive, and keyboardRotationSpeed if not
+        var cameraRotationSpeed = mouseControlsActive ? mouseRotationSpeedInDegrees : keyboardRotationSpeedInDegrees;
+        Debug.Log(cameraRotationSpeed);
+        //player head should follow camera slowly instead
+
+        /*        camera.transform.rotation = Quaternion.LookRotation(newDirection);*/
+/*        camera.transform.rotation = Quaternion.RotateTowards(transform.rotation, transform.rotation * rotationDirection, cameraRotationSpeed);*/
+        camera.transform.Rotate(rotationDirection.eulerAngles.x, rotationDirection.eulerAngles.y, 0); //TODO: This makes rotation too fast. Find a way to slow it
+
+        rollAngle = rollDirection * keyboardRotationSpeedInDegrees;
+        camera.transform.Rotate(transform.forward, rollAngle);
+
+
+        //TODO: need to turn player transform rotation to match camera transform rotation
+        /*Quaternion.RotateTowards(transform)*/
+        /*head.Rotate(transform.forward, 3);*/
+    }
+
+
     private void OnEnable()
     {
+        //obtain reference to head node (transforms will apply to head, and body segments will follow
+        
+
         //subscribe input actions to functions
         playerControls.Player.Boost.started += speedBoostStarted;
         playerControls.Player.Boost.canceled += speedBoostCancelled;
         playerControls.Player.Boost.Enable();
 
+        playerControls.Player.MouseCameraRotation.performed += mouseRotateCameraActionPerformed;
+        playerControls.Player.KeyboardCameraRotation.performed += keyboardRotateCameraActionPerformed;
+
+        playerControls.Player.MouseCameraRotation.canceled += RotateCameraActionCancelled;
+        playerControls.Player.KeyboardCameraRotation.canceled += RotateCameraActionCancelled;
+
+        if (mouseControlsActive)
+        {
+            playerControls.Player.MouseCameraRotation.Enable();
+        }
+        else
+        {
+            playerControls.Player.KeyboardCameraRotation.Enable();
+        }
+        playerControls.Player.Roll.performed += rollActionPerformed;
+        playerControls.Player.Roll.canceled += rollActionCancelled;
+        playerControls.Player.Roll.Enable();
     }
 
     private void OnDisable()
@@ -59,5 +111,49 @@ public class PlayerController: MonoBehaviour
         speedBoostActive = false;
     }
 
+    private void keyboardRotateCameraActionPerformed(InputAction.CallbackContext obj)
+    {
+        //rotate camera according to player turning speed
+        //SHOULD ONLY BE ACTIVE WHEN MOUSECONTROLS DISABLED
+        Vector2 rotationInput = playerControls.Player.KeyboardCameraRotation.ReadValue<Vector2>();
+        float currentRotationSpeed = keyboardRotationSpeedInDegrees;
+        rotationDirection = Quaternion.Euler(-rotationInput.y, rotationInput.x, 0);
+        /*rotationDirection = new Vector3(rotationInput.x, rotationInput.y, 0);*/
+        /*transform.rotation = Quaternion.RotateTowards(transform.rotation, transform.rotation * Quaternion.Euler(rotationInput.y, rotationInput.x, 0), currentRotationSpeed);*/
+
+    }
+    private void mouseRotateCameraActionPerformed(InputAction.CallbackContext obj)
+    {
+        //boost should not affect camera rotation speed, only player turning speed
+        Vector2 deltaMousePos = playerControls.Player.MouseCameraRotation.ReadValue<Vector2>();
+        Debug.Log(deltaMousePos);
+        //Rotates the camera towards the input
+        rotationDirection = Quaternion.Euler(-deltaMousePos.y, deltaMousePos.x, 0);
+        /*rotationDirection = new Vector3(deltaMousePos.x, deltaMousePos.y, 0);*/
+
+
+        /*        transform.rotation = Quaternion.RotateTowards(transform.rotation, transform.rotation * Quaternion.Euler(-deltaMousePos.y, deltaMousePos.x, 0), rotationSpeedInDegrees);*/
+
+        /*var rotationDirection = Vector3.RotateTowards(transform.forward, new Vector3(transform.position.x + movementVector.x, transform.position.y + movementVector.y, transform.position.z), rotationSpeedInDegrees, 0);*/
+        /*transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);*/
+
+        /*Debug.DrawRay(transform.position, rotationDirection, Color.red);
+        transform.rotation = Quaternion.LookRotation(rotationDirection);*/
+    }
+
+    private void RotateCameraActionCancelled(InputAction.CallbackContext obj)
+    {
+        rotationDirection = Quaternion.Euler(0, 0, 0);
+        /*rotationDirection = new Vector3();*/
+    }
+    private void rollActionPerformed(InputAction.CallbackContext obj)
+    {
+        rollDirection = playerControls.Player.Roll.ReadValue<float>();
+        /*transform.Rotate(transform.forward, rollDirection * keyboardRotationSpeedInDegrees, Space.World);*/
+    }
+    private void rollActionCancelled(InputAction.CallbackContext obj)
+    {
+        rollDirection = 0;
+    }
 
 }
